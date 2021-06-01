d4ec95e482f919c75495234db60adc86596f7170
916anan <40131953+916anan@users.noreply.github.com>
2021-06-01 14:49:29 +0800

    [新增功能](main): 文件

---
+++ pkg/file/files.go
@@ -0,0 +1,66 @@
+package file
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// Pwd get the current directory
+func Pwd() (string, error) {
+	return os.Getwd()
+}
+
+// MakeSureFile create if the file does not exist
+func MakeSureFile(f string) error {
+	err := FileIsOK(f)
+	if err != nil {
+		if err != nil {
+			err := os.MkdirAll(filepath.Dir(f), fs.ModePerm)
+			log.Printf("mkdir %s ,%v", filepath.Dir(f), err)
+			if err != nil {
+				return err
+			}
+		}
+		_, err = os.Create(f)
+		if err != nil {
+			log.Printf("create file err: %v", err)
+			return err
+		}
+	}
+	return nil
+}
+
+// FileIsOK exists and is a file
+func FileIsOK(f string) error {
+	if f == "" {
+		return fmt.Errorf("IsFileExists file must be not empty")
+	}
+	s, err := os.Stat(f)
+	if err != nil {
+		log.Printf("IsFileExists %s not exists", f)
+		return err
+	}
+	if s.IsDir() {
+		return fmt.Errorf("IsFileExists %s is a directory", f)
+	}
+	return nil
+}
+
+// DirIsOK exists and is a folder
+func DirIsOK(d string) error {
+	if d == "" {
+		return fmt.Errorf("DirIsOK dir must be not empty")
+	}
+	s, err := os.Stat(d)
+	if err != nil {
+		log.Printf("DirIsOK %s not exists", d)
+		return err
+	}
+	if !s.IsDir() {
+		return fmt.Errorf("DirIsOK %s is not a directory", d)
+	}
+	return nil
+}
